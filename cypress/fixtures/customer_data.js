c598fa909222e91f631a10c4c00b49adec08f19a
Irena <irena.jankova@email.cz>
2025-08-06 17:57:29 +0200

    Filling out appointment form succesfully

---
+++ cypress/fixtures/customer_data.js
@@ -0,0 +1,9 @@
+export const customerData = [
+  { label: 'Jméno a příjmení', selector: 'input[type="text"]', value: 'Jan Janko' },
+  { label: 'Email', selector: 'input[type="email"]', value: 'testmail@email.cz' },
+  { label: 'Telefon', selector: 'input[type="tel"]', value: '777 777 777' },
+];
+
+// Jmeno a prijmeni: Jan Janko
+// Email: testmail@email.cz
+// Telefon: 777 777 777
